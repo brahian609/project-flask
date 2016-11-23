4f11a55b04ae42010d073a07ad73c0bfb66a489d
Brahian Giraldo Medina <brahian609@hotmail.com>
2016-11-23 17:57:42 -0500

    add comment

--- app/views.py
+++ app/views.py
@@ -6,8 +6,10 @@ from app import app
 
 @app.route('/')
 def index():
+    """ Render a la vista index del projecto """
     return render_template("index.html")
 
 @app.route('/about')
 def about():
+    """ Render a la vista about del projecto """
     return render_template("about.html")
